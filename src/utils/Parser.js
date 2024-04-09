b90e6f2dc5d1e85a84399eb4007a49215b04cbfc
TangoBeee <zonebattle90@gmail.com>
2024-04-09 12:49:38 +0530

    fix: Add width to reactflow nodes

--- src/utils/Parser.js
+++ src/utils/Parser.js
@@ -32,6 +32,7 @@ export const parseJson = ( data, setNodes, setEdges, setError ) => {
                             id: parentNode,
                             data: { label: parentNode },
                             position,
+                            style: { width: 'fit-content' },
                         });
                     }
 
@@ -40,6 +41,7 @@ export const parseJson = ( data, setNodes, setEdges, setError ) => {
                             id: currentNode,
                             data: { label: currentNode },
                             position,
+                            style: { width: 'fit-content' },
                         });
                     }
 
